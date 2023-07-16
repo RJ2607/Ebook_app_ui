977dfb2f86ddfc2cc5d1bd48c316cf9ea6fa84c1
RJ2607 <rishijain2607@gmail.com>
2023-07-16 22:36:49 +0530

    profile

--- lib/my_profile.dart
+++ lib/my_profile.dart
@@ -14,37 +14,36 @@ class MyProfile extends StatefulWidget {
 class _MyProfileState extends State<MyProfile> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: color_ui.background,
-      child: Scaffold(
-        body: Column(
-          children: [
-            Expanded(
-              flex: 2,
-              child: Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CircleAvatar(
-                        radius: 70,
-                        backgroundImage: AssetImage("img/990130.jpg"),
-                        child: Stack(children: [
-                          Align(
-                            alignment: Alignment.bottomRight,
-                            child: CircleAvatar(
-                              radius: 22,
-                              backgroundColor: Colors.white70,
-                              child: Icon(CupertinoIcons.camera),
-                            ),
+    return Scaffold(
+      backgroundColor: Colors.red,
+      body: Column(
+        children: [
+          Row(),
+          Expanded(
+            flex: 2,
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  CircleAvatar(
+                      radius: 70,
+                      backgroundImage: AssetImage("img/990130.jpg"),
+                      child: Stack(children: [
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: CircleAvatar(
+                            radius: 22,
+                            backgroundColor: Colors.white70,
+                            child: Icon(CupertinoIcons.camera),
                           ),
-                        ]))
-                  ],
-                ),
+                        ),
+                      ]))
+                ],
               ),
             ),
-            Expanded(flex: 4, child: Container())
-          ],
-        ),
+          ),
+          Expanded(flex: 3, child: Container())
+        ],
       ),
     );
   }
